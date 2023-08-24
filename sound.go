9ed9b1e316bae8f15518ddf6f2601fea4d4fa5b6
cbott <cbott6@gmail.com>
2023-08-23 22:56:01 -0700

    initial sound channel structure

--- sound.go
+++ sound.go
@@ -1,16 +1,18 @@
 package main
 
-// Audio register naming: NRxy
+// Control for the Audio Processing Unit (APU)
+
+// Noise Register naming: NRxy
 // x = channel number (1-4,  5 = global)
 // y = register ID
 const (
-	// Channel 1: Pulse with period sweep
+	// Channel 1: Pulse A (with period sweep)
 	NR10 = 0xFF10 // Channel 1 sweep
 	NR11 = 0xFF11 // Channel 1 length timer and duty cycle
 	NR12 = 0xFF12 // Channel 1 volume and envelope
 	NR13 = 0xFF13 // Channel 1 period low
 	NR14 = 0xFF14 // Channel 1 period high and control
-	// Channel 2: Pulse
+	// Channel 2: Pulse B
 	NR21 = 0xFF16 // Channel 2 length timer and duty cycle
 	NR22 = 0xFF17 // Channel 2 volume and envelope
 	NR23 = 0xFF18 // Channel 2 period low
@@ -32,5 +34,19 @@ const (
 	// Global sound controls
 	NR50 = 0xFF24 // Master volume and VIN panning
 	NR51 = 0xFF25 // Sound panning
-	NR52 = 0xFF26 // Sound on/off
 )
+
+const (
+	NR52            = 0xFF26 // Sound on/off
+	NR52_apu_enable = 1 << 7 // 0 = all sound off
+	NR52_ch4_on     = 1 << 3 // read only
+	NR52_ch3_on     = 1 << 2 // read only
+	NR52_ch2_on     = 1 << 1 // read only
+	NR52_ch1_on     = 1 << 0 // read only
+)
+
+// Audio Processing Unit
+type APU struct {
+	// 0 = All sound off
+	on bool
+}
